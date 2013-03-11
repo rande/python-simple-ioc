8d7a0931f5a21bd7a403982a6ec970073e45b376
Thomas Rabaix <thomas.rabaix@ekino.com>
2013-03-11 02:28:52 +0100

    initial commit

---
+++ tests/ioc/service.py
@@ -0,0 +1,10 @@
+
+
+class Fake(object):
+    def __init__(self, mandatory, param=None):
+        self.mandatory = mandatory
+        self.param = param
+
+class Foo(object):
+    def __init__(self, fake):
+        self.fake
