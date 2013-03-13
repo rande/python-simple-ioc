c031577e5493dbfde74b9505dd6ad6658c4248f2
Thomas Rabaix <thomas.rabaix@gmail.com>
2013-03-13 01:35:45 +0100

    Add package information

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from setuptools import setup
+
+setup(
+    name="ioc",
+    version="0.0.1",
+    description="A small dependency injection container based on Symfony2 Dependency Component",
+    author="Thomas Rabaix",
+    author_email="thomas.rabaix@gmail.com",
+    url="https://github.com/rande/python-simple-ioc",
+    py_modules=["ioc"],
+    packages = ['ioc'],
+    install_requires=["pyyaml"],
+)
