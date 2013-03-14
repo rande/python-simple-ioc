846084dbf5104a4de06d0529a6122c565c491e24
Thomas Rabaix <thomas.rabaix@ekino.com>
2013-03-15 00:34:37 +0100

    Add logger

--- ioc/component.py
+++ ioc/component.py
@@ -35,8 +35,9 @@ class ParameterHolder(object):
         del self.parameters[key]
 
 class ParameterResolver(object):
-    def __init__(self):
+    def __init__(self, logger=None):
         self.re = re.compile("%%|%([^%\s]+)%")
+        self.logger = logger
 
     def resolve(self, parameter, parameter_holder):
         if not type(parameter) == str:
@@ -47,6 +48,9 @@ class ParameterResolver(object):
                 parameter[key] = self.resolve(parameter[key], parameter_holder)
 
         elif parameter[0:1] == '%' and parameter[-1] == '%' and parameter[1:-1] in parameter_holder.parameters:
+            if self.logger:
+                self.logger.debug("Match parameter: %s" % parameter[1:-1])
+
             return parameter_holder[parameter[1:-1]]
 
         else:
@@ -55,6 +59,9 @@ class ParameterResolver(object):
                     return '%'
 
                 return parameter_holder[matchobj.group(1)]
+
+            if self.logger:
+                self.logger.debug("Start resolving parameter: %s" % parameter)
 
             parameter, num = re.subn(self.re, replace, parameter)
 
@@ -80,15 +87,22 @@ class Container(object):
         return self.services[id]
 
 class ContainerBuilder(Container):
-    def __init__(self):
+    def __init__(self, logger=None):
         self.services = {}
         self.parameters = ParameterHolder()
         self.stack = []
-        self.parameter_resolver = ioc.component.ParameterResolver()
+        self.logger = logger
+        self.parameter_resolver = ioc.component.ParameterResolver(logger=logger)
 
     def build_container(self, container):
+        if self.logger:
+            self.logger.debug("Start building the container")
+
         for id, definition in self.services.iteritems():
             self.get_service(id, definition, container)
+
+        if self.logger:
+            self.logger.debug("Building container is over!")
 
     def get_class(self, definition):
         class_name = definition.klass.split(".")[-1]
@@ -99,23 +113,37 @@ class ContainerBuilder(Container):
         return getattr(m, class_name)
 
     def get_instance(self, klass, definition, container):
-        return klass(*self.set_services(definition.arguments, container), **self.set_services(definition.kwargs, container))
+
+        if self.logger:
+            self.logger.debug("Create instance for %s" % klass)
+
+        instance = klass(*self.set_services(definition.arguments, container), **self.set_services(definition.kwargs, container))
+
+        return instance
 
     def get_service(self, id, definition, container):
+
+        if self.logger:
+            self.logger.debug("Get service: id=%s, class=%s" % (id, definition.klass))
+
         if container.has(id):
             return container.get(id)
 
         if id in self.stack:
+            if self.logger:
+                self.logger.error("ioc.exceptions.CyclicReference: " + " -> ".join(self.stack) + " -> " + id)
+
             raise ioc.exceptions.CyclicReference(" -> ".join(self.stack) + " -> " + id)
 
         self.stack.append(id)
         instance = self.get_instance(self.get_class(definition), definition, container)
-        container.add(id, instance)     
+        container.add(id, instance)
         self.stack.pop()
 
         return instance
 
     def set_service(self, value, container):
+        
         if isinstance(value, Reference) and not container.has(value.id):
             return self.get_service(value.id, self.get(value.id), container)
 
@@ -128,12 +156,7 @@ class ContainerBuilder(Container):
         return self.parameter_resolver.resolve(value, self.parameters)
 
     def set_services(self, arguments, container):
-
         for pos in ioc.helper.get_keys(arguments):
             arguments[pos] = self.set_service(arguments[pos], container)
 
         return arguments
- 
-
-
-
