8b261216a97794fe23f31ff1af71de5080414780
Thomas Rabaix <thomas.rabaix@gmail.com>
2013-03-17 22:44:46 +0100

    Add support for method call

--- tests/ioc/service.py
+++ tests/ioc/service.py
@@ -4,6 +4,12 @@ class Fake(object):
     def __init__(self, mandatory, param=None):
         self.mandatory = mandatory
         self.param = param
+        self.ok = True
+        self.arg2 = True
+
+    def set_ok(self, ok, arg2=None):
+        self.arg2 = arg2
+        self.ok = ok
 
 class Foo(object):
     def __init__(self, fake):
