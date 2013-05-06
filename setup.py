81cb024af11ce6007d15b21a5d905b1fa76cbf6c
Thomas Rabaix <thomas.rabaix@gmail.com>
2013-05-06 21:22:51 +0200

    update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name="ioc",
-    version="0.0.5",
+    version="0.0.6",
     description="A small dependency injection container based on Symfony2 Dependency Component",
     author="Thomas Rabaix",
     author_email="thomas.rabaix@gmail.com",
