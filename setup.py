711b4d28fbda08bec744a6a07d982ad13869eff6
Thomas Rabaix <thomas.rabaix@ekino.com>
2013-08-20 23:08:42 +0200

    Update version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="ioc",
-    version="0.0.12",
+    version="0.0.13",
     description="A small dependency injection container based on Symfony2 Dependency Component",
     author="Thomas Rabaix",
     author_email="thomas.rabaix@gmail.com",
